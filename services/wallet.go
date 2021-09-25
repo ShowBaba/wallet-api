5f52dae89e6178166871bccb9d13545006c33eee
Sam <samwise858@gmail.com>
2021-09-25 17:01:05 +0100

    Update wallet.go

--- services/wallet.go
+++ services/wallet.go
@@ -71,8 +71,7 @@ import (
 )
 
 
-var InfuraUrl = "https://mainnet.infura.io/v3/f9cbc1ba4e27408b831c2adde76d2293" // mainnet
-// var InfuraUrl = "https://kovan.infura.io/v3/342f979e9d594a0ea51404cf3841eafa" // kovan testnet
+var InfuraUrl = "https://kovan.infura.io/v3/342f979e9d594a0ea51404cf3841eafa" // kovan testnet
 // var ganacheUrl = "HTTP://127.0.0.1:8545"
 
 func CreateWallet() (*string, error) {
